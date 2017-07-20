3fa41c42550862b20a2179bdb8923627dbe3649d
Haby Randall <sow.haby@gmail.com>
2017-07-20 12:08:09 -0700

    Redux boiler plate

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
