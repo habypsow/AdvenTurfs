74b81f1d1b8d17d5a4686b03230849ea9f5fdc63
Haby Randall <sow.haby@gmail.com>
2017-07-21 11:49:39 -0700

    Trying to call api

--- actions/park_actions.js
+++ actions/park_actions.js
@@ -5,11 +5,23 @@ import {
   FETCH_PARKS
 } from './types';
 
+const ROOT_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
 
 const PARK_QUERY_PARAMS = {
-  
+  key: 'AIzaSyDl3wgOiDflGbUlR6aTuWkb0-VdaJC3PLY',
+  location: '47.6097,-122.3331',
+  radius: '100',
+  type: 'park'
+};
+
+const buildUrl = () => {
+  const query = qs.stringify({ ...PARK_QUERY_PARAMS });
+  return `${ROOT_URL}${query}`;
+
 }
 
 export const fetchParks = (region) => async (dispatch) => {
-
+let { data } = await axios.get(buildUrl);
+dispatch({ type: FETCH_PARKS, payload: data });
+console.log(data);
   };
