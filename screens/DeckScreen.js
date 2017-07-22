5571a87d0b20c766c409a3a722af998396ac834b
Haby Randall <sow.haby@gmail.com>
2017-07-22 15:15:46 -0700

    App crashing when I specify park coordinates from api

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -9,13 +9,30 @@ import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
   renderCard(park) {
+
+    // const initialRegion = {
+    //   longitude: park.geometry.location.lng,
+    //   latitude: park.geometry.location.lat,
+    //   latitudeDelta: 0.045,
+    //   longitudeDelta: 0.02
+    // }
+  console.log("Heeeyyy" + this.props);
     return (
       <Card title={park.name}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={true}
+            // initialRegion={initialRegion}
+            >
+          </MapView>
+        </View>
         <View style={styles.detailWrapper}>
           <Text>{park.vicinity}</Text>
           <Text>{park.rating}</Text>
         </View>
-    
+
       </Card>
     )
   }
@@ -28,11 +45,12 @@ class DeckScreen extends Component {
 
   render() {
     return (
-      <View>
+      <View style={{ marginTop: 10 }}>
       <Swipe
       data={this.props.parks}
       renderCard={this.renderCard}
       renderNoMoreCards={this.renderNoMoreCards}
+
       />
       </View>
     );
