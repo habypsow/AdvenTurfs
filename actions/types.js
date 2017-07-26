5ce0fbda8856644fd95dd279e742843a3d02c6d3
Haby Randall <sow.haby@gmail.com>
2017-07-26 10:14:19 -0700

    Reset button now working

--- actions/types.js
+++ actions/types.js
@@ -2,3 +2,4 @@ export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
 export const FETCH_PARKS = 'fetch_parks';
 export const LIKE_PARK = 'like_park';
+export const CLEAR_LIKED_PARKS = 'clear_liked_parks';
