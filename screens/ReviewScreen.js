0e883e5dd190d6dddf6f8045b1ee307a7cce8edc
Haby Randall <sow.haby@gmail.com>
2017-07-26 00:18:41 -0700

    Working on review screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, ScrollView, Linking } from 'react-native';
+import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 
 class ReviewScreen extends Component {
@@ -14,16 +15,60 @@ class ReviewScreen extends Component {
     color="rgba(0,122,255,1)"/>
   });
 
+renderLikedParks() {
+  return this.props.likedParks.map(park => {
+    const initialRegion = {
+      longitude: park.geometry.location.lng,
+      latitude: park.geometry.location.lat,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
+    return (
+      <Card title={park.name} key={park.id}>
+        <View style={{ height: 200 }}>
+          <MapView
+            style={{ flex: 1 }}
+            cacheEnabled={true}
+            scrollEnabled={false}
+            initialRegion={initialRegion}
+          />
+          <View style={styles.detailWrapper}>
+            <Text style={styles.italics}>{park.name}</Text>
+            <Text style={styles.italics}>{park.rating}</Text>
+          </View>
+          <Button title="Get Directions"
+          backgroundColor="#03A9F4"
+          onPress={() => Linking.openURL('https://maps.google.com/')}
+          />
+        </View>
+      </Card>
+    );
+  });
+}
 
 
   render() {
     return (
-      <View>
-      
-      </View>
+      <ScrollView>
+        {this.renderLikedParks()}
+      </ScrollView>
     );
   }
 }
+
+const styles = {
+  italics: {
+    fontStyle: 'italic'
+  },
+  detailWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10
+  }
+}
+
 function mapStateToProps(state) {
   return { likedParks: state.likedParks };
 }
